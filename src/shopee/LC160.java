7ee96c1e9e4d5984db3f94e41ab19d388f0a8cb8
fanxinwei <fanxinwei@yuanbaobaoxian.com>
2021-08-17 19:35:36 +0800

    MOD:最近的一些练习

--- src/shopee/LC160.java
+++ src/shopee/LC160.java
@@ -1,7 +1,57 @@
 package shopee;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LC160 {
 
+    public static void main(String[] args) {
+        /*ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        // 交点
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        ListNode l6 = new ListNode(6);
+        ListNode l7 = new ListNode(7);
+        ListNode l8 = new ListNode(8);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l7;
+        l7.next = l8;
+        l4.next = l5;
+        l5.next = l6;
+        l5.next = l3;*/
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(1);
+        ListNode intersectionNode = getIntersectionNode(l1, l2);
+        System.out.println(intersectionNode);
+    }
 
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        List<ListNode> linkRecord = new ArrayList<>();
+        ListNode res = null;
+        while (headB != null) {
+            linkRecord.add(headB);
+            headB = headB.next;
+        }
+        while (headA != null) {
+            if (linkRecord.contains(headA)) {
+                res = headA;
+                break;
+            }
+            headA = headA.next;
+        }
+        return res;
+    }
+
+    public static ListNode getIntersectionNode01(ListNode headA, ListNode headB) {
+        ListNode pa = headA, pb = headB;
+        while (pa != pb) {
+            pa = pa == null ? headB : pa.next;
+            pb = pb == null ? headA : pb.next;
+        }
+        return pa;
+    }
 
 }
---
+++ src/shopee/ListNode.java
@@ -0,0 +1,16 @@
+package shopee;
+
+public class ListNode {
+
+    int val;
+    ListNode next;
+    ListNode(int _val) {
+        val = _val;
+        next = null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
