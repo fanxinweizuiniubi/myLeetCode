886ace2c21e0951e478ed914d3494de35502d958
fanxinwei <fanxinwei@yuanbaobaoxian.com>
2021-11-17 19:14:03 +0800

    mod: practice

---
+++ src/basic/ListNode.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class ListNode {
+
+    int val;
+
+    ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/linkedList/ReverseLinkedList.java
@@ -0,0 +1,16 @@
+package linkedList;
+
+public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null, cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+}
