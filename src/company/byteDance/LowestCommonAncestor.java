d152b4b50ea6f4dd6ec835a5795cd4d581db21f4
fanxinwei <fanxinwei@yuanbaobaoxian.com>
2021-12-13 10:59:06 +0800

    mod: byteDance

---
+++ src/company/byteDance/LowestCommonAncestor.java
@@ -0,0 +1,16 @@
+package company.byteDance;
+
+import basic.TreeNode;
+
+public class LowestCommonAncestor {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) return right;
+        if (right == null) return left;
+        return root;
+    }
+
+}
